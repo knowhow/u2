2fb5763bd3e1890c63170d91695d721fc91dbd3b
Ernad Husremovic <hernad@bring.out.ba>
2011-10-13 17:32:48 +0200

    expan tabs u vim-u

--- database/misc/checkPackageVersion.sql
+++ database/misc/checkPackageVersion.sql
@@ -10,10 +10,10 @@
 --
 
 -- usage:  
--- find check if xtpos package is 3.x.x version                       : select package_is_version('xtpos', '3.[0-9].[0-9]');
--- find check if xtpos package is 3.x and patch number is 1 or 2 or 3 : package_is_version('xtpos', '3.[0-9].[123]');
+-- find check if xtpos package is 3.x.x version                       : select u2.package_is_version('xtpos', '3.[0-9].[0-9]');
+-- find check if xtpos package is 3.x and patch number is 1 or 2 or 3 : select u2.package_is_version('xtpos', '3.[0-9].[123]');
 -- find check if xtpos package is 3.x and patch number is 1 or 2 or 3,
---  after patch number we could have arbitrary string (rc1, beta2 etc): package_is_version('xtpos', '3.[0-9].[123].*');
+--  after patch number we could have arbitrary string (rc1, beta2 etc): select u2.package_is_version('xtpos', '3.[0-9].[123].*');
 
 
 CREATE OR REPLACE FUNCTION package_is_version(package_name text, regexp_version text) RETURNS boolean AS $$
@@ -21,14 +21,14 @@ CREATE OR REPLACE FUNCTION package_is_version(package_name text, regexp_version 
 DECLARE cnt integer;
 
 BEGIN
+    
+    SELECT count(*) INTO cnt FROM pkghead WHERE pkghead_name = package_name AND pkghead_version ~ regexp_version;
 
-	 SELECT count(*) INTO cnt FROM pkghead WHERE pkghead_name = package_name AND pkghead_version ~ regexp_version;
-
-     if cnt > 0 THEN
-           return true;
-	 ELSE
-		   return false;
-	 END IF;
+    if cnt > 0 THEN
+        return true;
+    ELSE
+        return false;
+    END IF;
 
 END;
 $$ LANGUAGE plpgsql;
