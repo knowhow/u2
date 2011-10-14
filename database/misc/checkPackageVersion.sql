0f09e5d9e31b4d60ffc4e07cab62139f43e11f2d
Ernad Husremovic <hernad@bring.out.ba>
2011-10-14 09:39:07 +0200

    ver 0.2.0

--- database/misc/checkPackageVersion.sql
+++ database/misc/checkPackageVersion.sql
@@ -32,3 +32,32 @@ BEGIN
 
 END;
 $$ LANGUAGE plpgsql;
+
+--  xtpos ver '3.7.2beta1':  select package_version('xtpos') -> 3*10_000+7*100+2 = 30702 (numeric)
+--            '34.12.54'                                     -> 34*10_000 + 12*100 + 54 = 341254
+--            '34.2.3'                                       -> 34*10_000 + 2*100 + 3 = 340203
+
+CREATE OR REPLACE FUNCTION package_version(package_name text) RETURNS integer AS $$
+
+DECLARE 
+pkg_ver text;
+v_major integer;
+v_minor integer;
+v_patch integer;
+
+BEGIN
+        
+    SELECT pkghead_version INTO pkg_ver FROM pkghead WHERE pkghead_name = package_name;
+
+    if NOT FOUND THEN
+        return 0;
+    ELSE
+        v_major = SUBSTRING(pkg_ver FROM '(\d+).\d+.\d+.*');
+        v_minor = SUBSTRING(pkg_ver FROM '\d+.(\d+).\d+.*');
+        v_patch = SUBSTRING(pkg_ver FROM '\d+.\d+.(\d+).*');
+            
+        return v_major * 10000 + v_minor * 100 + v_patch;
+    END IF; 
+
+END;
+$$ LANGUAGE plpgsql;
