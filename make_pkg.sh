43216e510727b535d775eed826d50abf490973c6
Ernad Husremovic <hernad@bring.out.ba>
2011-10-26 17:11:58 +0200

    OS gnutar/tar

--- make_pkg.sh
+++ make_pkg.sh
@@ -3,6 +3,14 @@
 PKG_NAME=u2
 PKG_DIR=u2
 
+OS_MAC_OSX = `uname -a | grep -c Darwin`
+
+if [ OS_MAC_OSX == "1" ]; then
+    TAR=gnutar
+else
+    TAR=tar
+fi
+
 mkdir package
 gnutar cvfz package/$PKG_NAME.gz ../$PKG_DIR/ --exclude=*.gz --exclude=.git
 
