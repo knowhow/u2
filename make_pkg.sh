68647448f6af9f8315ba0960f511222307a30b6f
vsasa <sasa.vranic@bring.out.ba>
2013-04-05 08:56:41 +0200

    u2, make_pkg skripta ispravljena

--- make_pkg.sh
+++ make_pkg.sh
@@ -3,18 +3,19 @@
 PKG_NAME=u2
 PKG_DIR=u2
 
-OS_MAC_OSX = `uname -a | grep -c Darwin`
+OS_DARWIN=`uname -a | grep -c Darwin`
 
-if [ OS_MAC_OSX == "1" ]; then
+echo $OS_DARWIN
+
+if [ "$OS_DARWIN" != "0" ]; then
     TAR=gnutar
 else
     TAR=tar
 fi
 
 mkdir package
-
+rm package/$PK_NAME.gz
 cd ..; $TAR cvfz $PKG_DIR/package/$PKG_NAME.gz  --exclude="*/.gitignore" --exclude="*/package" --exclude="*.gz" --exclude="*/.git" $PKG_DIR
-
-echo "----- result:  ----"
 cd $PKG_DIR
+echo "----- dobili smo:  ----"
 ls package/$PKG_NAME.gz
